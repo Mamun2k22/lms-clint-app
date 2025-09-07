7828cc3a9f1e340de3a46b8f7c2523f5615a0115
Mamun2k22 <mamun180037@gmail.com>
2025-09-07 12:03:33 +0600

    remove some css

--- .next/static/chunks/OneDrive_Desktop_Parrale task_lms client_src_app_favicon_ico_mjs_1df5b4fc._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/0bbf2_next_dist_ebd0b712._.js"
-],
-    source: "dynamic"
-});
